17ed8d12b78b445be47087fed535a6533cbe9a69
Tomasz S <tsyrylo1@gmail.com>
2023-08-30 15:32:36 +0200

    v1

--- Snake-v1/Snake_v1.py
+++ Snake-v1/Snake_v1.py
@@ -1,7 +1,12 @@
 import pygame
 import random
+from pygame import mixer
+
 
 pygame.init()
+mixer.music.load('extreme-metal.mp3')
+
+mixer.music.set_volume(0.2)
 
 # Colors
 white = (255, 255, 255)
@@ -56,10 +61,12 @@ def game_loop():
             dis.fill(black)
             draw_text("You Lost! Press C to Play Again or Q to Quit", red, dis_width / 6, dis_height / 4)
             draw_text("Your Score: " + str(length_of_snake - 1), white, dis_width / 3, dis_height / 3)
+            mixer.music.pause()
             pygame.display.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
+                   
                     if event.key == pygame.K_q:
                         game_over = True
                         game_close = False
@@ -78,6 +85,7 @@ def game_loop():
                 }
                 if event.key in key_actions:
                     x1_change, y1_change = key_actions[event.key]
+                    mixer.music.play()
 
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
@@ -99,6 +107,7 @@ def game_loop():
 
         draw_text("Your Score: " + str(length_of_snake - 1), red, 10, 10)
         draw_text("Level: " + str(level), red, dis_width - 100, 10)
+        
 
         pygame.display.update()
 
